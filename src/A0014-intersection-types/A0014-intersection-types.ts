bf883588a76b55d0a6cd0ad9cf6d33a2cf384b0f
Éverton Carvalho <evertonsnake@gmail.com>
2023-11-21 08:33:26 -0300

    A0014-Intersection-types

---
+++ src/A0014-intersection-types/A0014-intersection-types.ts
@@ -0,0 +1,19 @@
+type TemNome = { nome: string };
+type TemSobrenome = { sobrenome: string };
+type TemIdade = { idade: number };
+
+type Pessoa = TemNome & TemSobrenome & TemIdade;
+
+type AB = 'A' | 'B';
+type AC = 'A' | 'C';
+type AD = 'D' | 'A';
+type Intersecao = AB & AC & AD;
+
+const pessoa: Pessoa = {
+  nome: 'ea',
+  sobrenome: 'ca',
+  idade: 30
+};
+
+console.log(pessoa);
+export { pessoa };
